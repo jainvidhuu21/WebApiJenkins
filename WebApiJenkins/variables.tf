d5b795d49d18bf789ee424aa5a5fae1ec75c9b36
Vidhi Jain <113572796+jainvidhuu21@users.noreply.github.com>
2025-04-04 15:46:50 +0530

    Update variables.tf

--- WebApiJenkins/variables.tf
+++ WebApiJenkins/variables.tf
@@ -1,1 +1,44 @@
 
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "jenkins-vidhi-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "app_service_plan_name" {
+  description = "App Service Plan name"
+  type        = string
+  default     = "jenkins-vidhi-plan"
+}
+
+variable "app_service_name" {
+  description = "App Service (Web App) name"
+  type        = string
+  default     = "jenkins-vidhi-app123"
+}
