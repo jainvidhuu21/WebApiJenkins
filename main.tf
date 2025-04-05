fb92b8a9523260f74a150241083dd32db342a810
Vidhi Jain <113572796+jainvidhuu21@users.noreply.github.com>
2025-04-05 12:44:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.26.0"  
-    }
-  }
-}
-
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 # Resource Group
@@ -19,12 +15,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # App Service Plan
-resource "azurerm_service_plan" "plan" {
+resource "azurerm_app_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
-  sku_name            = "B1"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
 # App Service (Web App)
@@ -32,7 +31,7 @@ resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_service_plan.plan.id
+  app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
     always_on = true
