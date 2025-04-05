27b1b0214d60a0bf4b3843532964abbfcb580fe6
Vidhi Jain <113572796+jainvidhuu21@users.noreply.github.com>
2025-04-05 12:33:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.26.0"  
     }
   }
 }
+
 
 provider "azurerm" {
   features {}
