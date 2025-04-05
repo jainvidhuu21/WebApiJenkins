0e346bc4912acbf890245f330c3d54fe30edb92a
Vidhi Jain <113572796+jainvidhuu21@users.noreply.github.com>
2025-04-05 12:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  app_service_plan_id = azurerm_service_plan.plan.id
 
   site_config {
     always_on = true
